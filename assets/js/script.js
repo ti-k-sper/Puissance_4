c41054f37356ae242c80c156d1a3f9f6fc9312dc
ti-k-sper <y.chargueraud@hotmail.fr>
2019-05-07 14:08:03 +0200

    js 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var numeroJoueur = 1;
+var joueur = 1;
 var nbColonne = 5;
 var nbLigne = 5;
 var jeu = true;
@@ -8,7 +8,6 @@ var plateauJeu = [];
 for (i = 0; i < nbLigne; i++) {
 	plateauJeu[i] = new Array();
 }
-//console.log(plateauJeu);
 
 function newGame(){
 	for (i = 0; i < nbLigne; i++) {
@@ -16,11 +15,10 @@ function newGame(){
 			plateauJeu[i][j] = 0;
 			}
 		}
-	numeroJoueur = 1;
-	afficheTexteAnnonce("Le jeu commence! C'est au tour du joueur " + nomDujoueur(numeroJoueur));
+	joueur = 1;
+	afficheTexteAnnonce("Le jeu commence! C'est au tour du joueur " + nomDujoueur(joueur));
 	jeu = true;
-	//creerTableau();
-	//console.log('fin');
+	creerTableau();
 }
 
 function afficheTexteAnnonce(pTexte){
@@ -33,5 +31,25 @@ function nomDujoueur(pNumeroJoueur){
 		return('rouge');
 	}else{
 		return('bleu');
+	}
+}
+
+function creerTableau(){
+	<table> = texte;
+	for (i = 0; i < nbLigne; i++) {
+		texte = texte + <tr>;
+		for (j = 0; j < nbColonne; j++) {
+			texte = texte + <td onclick="detecteClick(j)" id="i-j">;
+			if(plateauJeu[i][j] == 1){
+				texte = texte + <div class="joueur1">;
+			}
+			if(plateauJeu[i][j] == 2){
+				texte = texte + <div class="joueur2">;
+			}
+			texte = texte + </td>;
+		}
+		texte = texte + </tr>;
+	texte = texte + </table>;
+	texte.innerHTML = document.getElementById('puissanceQuatre');
 	}
 }
